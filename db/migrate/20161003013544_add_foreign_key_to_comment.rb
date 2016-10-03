312dbc9224343d307f3d844b5680d86f3f0ddfc8
linguyen <nguyenvanlin01041994@gmail.com>
2016-10-03 14:33:31 +0700

    Final

--- db/migrate/20161003013544_add_foreign_key_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToComment < ActiveRecord::Migration[5.0]
-  def change
-  	add_reference :comments, :entry ,index:true
-  end
-end
--- db/migrate/20161003015229_add_reference_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToComment < ActiveRecord::Migration[5.0]
-  def change
-    add_column :comments, :entry, :reference
-  end
-end
